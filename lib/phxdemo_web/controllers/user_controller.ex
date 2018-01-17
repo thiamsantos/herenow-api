cf1836ce1745353aa53a8dca800519a3df2e8ac2
Thiago <thia.mdossantos@gmail.com>
2018-01-17 16:35:42 -0200

    :memo: docs

--- lib/phxdemo_web/controllers/user_controller.ex
+++ lib/phxdemo_web/controllers/user_controller.ex
@@ -6,6 +6,51 @@ defmodule PhxdemoWeb.UserController do
 
   action_fallback PhxdemoWeb.FallbackController
 
+  @apidoc """
+  @apiDefine UserNotFoundError
+
+  @apiError UserNotFound The id of the User was not found.
+
+  @apiErrorExample Error-Response:
+      HTTP/1.1 404 Not Found
+      {
+        "message": "Not Found"
+      }
+  """
+
+  @apidoc """
+  @apiDefine DefaultHeader
+
+  @apiHeader Content-Type application/json.
+  """
+
+  @apidoc """
+  @apiDefine AuthorizationHeader
+
+  @apiHeader Authorization Authorization Bearer token.
+  @apiHeaderExample {json} Header-Example:
+    {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1MTYyMTk5NjEsImlhdCI6MTUxNjIxMjc2MX0.ytDiDZ2EFua46ifCevXZH9YjL8CnlCPLoecIodD5d-w"
+    }
+  """
+
+  @apidoc """
+  @apiDefine InvalidAuthorizationError
+
+  @apiError InvalidAuthorization Invalid Token
+
+  @apiErrorExample Error-Response:
+      HTTP/1.1 401 Not Found
+      {
+        "message": "Invalid authorization"
+      }
+  """
+
+  @apidoc """
+  @apiDefine private User access only
+  Requires a authorization bearer token with a valid jwt.
+  """
+
   def index(conn, _params) do
     users = Users.list_users()
     render(conn, "index.json", users: users)
@@ -20,17 +65,29 @@ defmodule PhxdemoWeb.UserController do
     end
   end
 
-  # {
-  # @api {get} /user/:id Request User information
-  # @apiName GetUser
-  # @apiGroup User
+  @apidoc """
+  @api {get} /user/:id Request User information
+  @apiName GetUser
+  @apiGroup User
 
-  # @apiParam {Number} id Users unique ID.
+  @apiParam {Number} id Users unique ID.
 
-  # @apiSuccess {Number} id   User unique ID.
-  # @apiSuccess {Number} age  Age of the User.
-  # @apiSuccess {String} name Name of the User.
-  # }
+  @apiSuccess {Number} id   User unique ID.
+  @apiSuccess {Number} age  Age of the User.
+  @apiSuccess {String} name Name of the User.
+  @apiSuccessExample Success-Response:
+      HTTP/1.1 200 OK
+      {
+        "firstname": "John",
+        "lastname": "Doe"
+      }
+
+  @apiUse UserNotFoundError
+  @apiUse InvalidAuthorizationError
+  @apiUse AuthorizationHeader
+  @apiUse DefaultHeader
+  @apiPermission private
+  """
   def show(conn, %{"id" => id}) do
     user = Users.get_user!(id)
     render(conn, "show.json", user: user)
