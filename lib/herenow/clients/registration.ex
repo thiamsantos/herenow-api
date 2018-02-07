63bbebe2de279fb07968e9dcfbd9897932f069ae
Thiago <thia.mdossantos@gmail.com>
2018-02-07 16:57:31 -0200

    :sparkles: write main flux

---
+++ lib/herenow/clients/registration.ex
@@ -0,0 +1,18 @@
+defmodule Herenow.Clients.Registration do
+  defstruct [
+    :address_number,
+    :cep,
+    :city,
+    :email,
+    :is_company,
+    :is_verified,
+    :legal_name,
+    :name,
+    :password,
+    :segment,
+    :state,
+    :street,
+    :captcha
+  ]
+end
+
