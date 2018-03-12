56c344d68f1ddef2a4be13ec939632767679f19b
Thiago Santos <thia.mdossantos@gmail.com>
2018-03-12 11:16:07 -0300

    docs(apidoc): add documentation for existing endpoints

--- lib/herenow_web/controllers/auth_controller.ex
+++ lib/herenow_web/controllers/auth_controller.ex
@@ -5,6 +5,65 @@ defmodule HerenowWeb.AuthController do
 
   action_fallback(HerenowWeb.FallbackController)
 
+  # {
+  # @apiDefine InvalidCredentialsError
+  # @apiError InvalidCredentials Invalid credentials.
+  # @apiErrorExample InvalidCredentialsResponse:
+  #     HTTP/1.1 401 Unauthorized
+  #     {
+  #       "message": "Authorization failed!",
+  #       "errors": [
+  #         {
+  #           "message": "Invalid credentials",
+  #           "code": 301
+  #         }
+  #       ],
+  #       "code": 300
+  #     }
+  # }
+
+  # {
+  # @apiDefine AccountUnverifiedError
+  # @apiError AccountUnverified Account was not verified.
+  # @apiErrorExample AccountUnverifiedResponse:
+  #     HTTP/1.1 401 Unauthorized
+  #     {
+  #       "message": "Authorization failed!",
+  #       "errors": [
+  #         {
+  #           "message": "Account not verified",
+  #           "code": 302
+  #         }
+  #       ],
+  #       "code": 300
+  #     }
+  # }
+
+  # {
+  # @api {post} /auth/identity request an access token
+  # @apiName Authenticate
+  # @apiGroup Authentication
+  #
+  # @apiParam {String} email Client's email.
+  # @apiParam {String} password Client's password.
+  # @apiParam {String} captcha Recaptcha code.
+  #
+  # @apiSuccess {String} token Access token.
+  # @apiSuccessExample Success-Response:
+  #     HTTP/1.1 201 CREATED
+  #     {
+  #       "token": "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o"
+  #     }
+  #
+  # @apiUse DefaultHeader
+  # @apiUse MissingRequiredKeysError
+  # @apiUse InvalidKeyTypeError
+  # @apiUse InvalidCaptchaError
+  # @apiUse LongEmailError
+  # @apiUse InvalidEmailFormatError
+  # @apiUse InvalidCredentialsError
+  # @apiUse AccountUnverifiedError
+  # }
   def create(conn, params) do
     with {:ok, token} <- Clients.authenticate(params) do
       conn
